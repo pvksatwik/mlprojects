e7bc781f0e0614712dbf1e5ff6392a5aab349ca5
satwik <pasumarthipsatwik@gmail.com>
2024-05-21 10:05:11 +0530

    application created

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,16 +3,11 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-
-from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
-
-from src.components.model_trainer import ModelTrainerConfig
-from src.components.model_trainer import ModelTrainer
-
+from sklearn.model_selection import train_test_split
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
